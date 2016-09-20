47e47eeb4a9ee997a33f90dbd1d84ac2c6e23409
Justin <woodmansee1207@gmail.com>
2016-09-20 17:47:52 -0600

    filled out controllers

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,13 +1,44 @@
 class Api::ProfilesController < ApplicationController
+  before_action :set_profile, except: [:index, :create]
+
   def index
+    render json: Profile.all
   end
 
   def show
+    render json: @profile
+  end
+
+  def create
+    profile = Profile.new(profile_params)
+    if profile.save
+      render json: profile
+    else
+      render json: {errors: profile.errors}, status: 401
+    end
   end
 
   def update
+    if @profile.update(profile_params)
+      render json: @profile
+    else
+      render json: {errors: profile.errors}, status: 401
+    end
   end
 
   def destroy
+    @profile.destroy
+    render json: {message: 'Destroyed!'}
   end
+
+  private
+
+  def profile_params
+    params.require(:movie).permit(:zip_code, :bio, :inspirations, :user_id)
+  end
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
 end
--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,13 +1,49 @@
 class Api::ProjectsController < ApplicationController
+  before_action :set_profile, except: [:index, :update, :destroy]
+  before_action :set_project, except: [:index, :create]
+
   def index
+    render json: Project.all
   end
 
   def show
+    render json: @project
+  end
+
+  def create
+    project = @profile.project.new(project_params)
+    if project.save
+      render json: project 
+    else
+      render json: {errors: project.errors}, status: 401
+    end
   end
 
   def update
+    if @project.update(project_params)
+      render json: @project
+    else
+      render json: {errors: project.errors}, status: 401
+    end
   end
 
   def destroy
+    @project.destroy
+    render json: {message: 'Destroyed!'}
   end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :category, :collab, :active, :profile_id)
+  end
+
+  def set_profile
+    @profile = Profile.find(params[:profile_id])
+  end
+
+  def set_project
+    @project = Profile.project.find(params[:id])
+  end
+
 end
