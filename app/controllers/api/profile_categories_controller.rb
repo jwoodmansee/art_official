f59f7817bd4135e970b3b7b26d28ea81f55dda15
Alex Quan <alxquan@gmail.com>
2016-09-21 15:34:59 -0600

    added Profile_categories controller

--- app/controllers/api/profile_categories_controller.rb
+++ app/controllers/api/profile_categories_controller.rb
@@ -1,16 +1,42 @@
 class Api::ProfileCategoriesController < ApplicationController
+
   def index
+    render json: Profile_categories.all
   end
 
   def show
+    render json: @profile_categories
   end
 
   def create
+    profile_categories = Profile_categories.new(profile_categories_params)
+    if profile_categories.save_limit
+      render json: profile_categories
+    else
+      render json: {errors: profile_categories.errors}, status: 401
+    end
   end
 
   def update
+    if @profile_categories(profile_categories_params)
+      render json: @profile_categories
+    else
+      render json: {errors: profile_categories.errors}, status: 401
+    end
   end
 
   def destroy
+    @profile_categories.destroy
+    render json: {message: 'Destroyed!'}
+  end
+
+  private
+
+  def profile_categories_params
+    params.require(:profile_categories).permit(:music, :photography, :videography, :muralist, :painting, :drawing, :sculpture, :graphic_design, :performance, :literature, :hand_made, :profile_id)
+  end
+
+  def set_profile_categories
+    @profile_categories = Profile_categories.find(params[:id])
   end
 end
