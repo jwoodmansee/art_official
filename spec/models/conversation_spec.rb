85af8addd287b9858681a2aa6289329c2d264c94
Alex Quan <alxquan@gmail.com>
2016-09-22 17:03:11 -0600

    Setup testing ENV and started conversation_spec tests

---
+++ spec/models/conversation_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Conversation, type: :model do
+  # it 'has a specific subject' do
+  # subject = "This is the subject!"
+  # conversation = Conversation.create(subject: subject)
+  # expect(conversation.subject). to eq(subject)
+  describe 'attributes' do
+    it { should respond_to(:subject) }
+    it { should respond_to(:user_id) }
+    it { should belong_to(:user) }
+    binding.pry
+    it { should have_many(:messages) }
+  end
+end
+ß
