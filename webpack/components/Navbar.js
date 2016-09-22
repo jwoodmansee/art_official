957e220f588b79995913039636329e3dc3d5f410
Ann Hall <anniemaesato@gmail.com>
2016-09-22 11:43:53 -0600

    nav, routes.js, auth

--- webpack/components/Navbar.js
+++ webpack/components/Navbar.js
@@ -1,36 +1,65 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { handleLogout } from './auth/actions';
+import { connect } from 'react-redux';
 
 
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+  
+  logout(e) {
+    e.preventDefault();
+    this.props.dispatch(handleLogout(this.props.history));
+  }
 
-const Navbar = () => (
-  <nav className="navbar navbar-default">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <Link to='/' className="navbar-brand">
-          collabART
-        </Link>
-      </div>
-      <div className="nav navbar-nav navbar-right">
-        <ul className="nav navbar-nav">
-          <li>
-            <Link to='/'>
-              About
-              // <span className="sr-only">
-              //   (current)
-              // </span>
+  authLink() {
+    if (this.props.auth) {
+      return (
+        [
+          <li key="auth-link-0"><Link to="/admin">Admin</Link></li>,
+          <li key="auth-link-1"><a href="#" onClick={this.logout}>Logout</a></li>
+        ]
+      )
+    } else {
+      return(<li><Link to="/login">Login</Link></li>);
+    }
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link to='/' className="navbar-brand">
+              collabART
             </Link>
-          </li>
-          <li>
-            <Link to='/'>
-              login
-            </Link>
-          </li>
-        </ul>
-      </div>
+          </div>
+          <div className="nav navbar-nav navbar-right">
+            <ul className="nav navbar-nav">
+              <li>
+                <Link to='/'>
+                  Home // unauthenticated routes
+                </Link>
+                <Link to='/about'>
+                  About // unauthenticated routes
+                </Link>
+              </li>
+              { this.authLink() }
+              <li>
+                <Link to='/'>
+                  login
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-    </div>
-  </nav>
-)
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default Navbar;
--- webpack/routes.js
+++ webpack/routes.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, browserHistory } from 'react-router';
+import { UserAuthWrapper } from 'redux-auth-wrapper';
 import App from './containers/App';
-import Projects form './components/Projects';
+import Projects from './components/Projects';
+import Profiles from './components/Profiles';
 import NoMatch from './components/NoMatch';
+
+const UserIsAuthendticated = UserAuthWrapper({
+  authSelector: state => state.auth,
+  predicate: auth => auth.isAuthenticated,
+  redirectAction: () => handleLogout(browserHistory),
+  wrapperDisplayName: 'UserIsAuthendticated'
+});
 
 export default (
   <Route>
     <Route path="/" component={App} >
-      <IndexRoute component={Projects} />
+      <IndexRoute component={Home} />
+      <Route path='about' component={About} />
+      <Route path='login' component={Login} />
+      <Route path='admin' component={UserIsAuthendticated(Admin)} />
       <Route path="*" status={404} component={NoMatch}/>
     </Route>
   </Route>
 )
-
