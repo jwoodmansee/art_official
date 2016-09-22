ef15140fc5e3c10578997b67914606717597ff7b
Justin <woodmansee1207@gmail.com>
2016-09-22 15:26:27 -0600

    added coded to the sessions and registration contollers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-before_action :configure_sign_up_params, only: [:create]
-before_action :configure_account_update_params, only: [:update]
+  respond_to :json
+  skip_before_action :verify_authenticity_token
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  skip_before_action :verify_authenticity_token
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
