abda125ec076e0a4f4c91dc9b884c5697c0c8a5d
Alex Quan <alxquan@gmail.com>
2016-09-23 11:47:40 -0600

    Message spec and conversation spec at 100 percent coverage

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe 'attributes' do
 
-    it { should validate_presence_of(:body) }
+    it { should respond_to(:body) }
     it { should respond_to(:user_id) }
     it { should respond_to(:conversation_id) }
     it { should belong_to(:user) }
+    it { should belong_to(:conversation) }
   end
 end
