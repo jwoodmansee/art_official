b1cef923ce109668047715af28ecfc57fc413e3a
Ann Hall <anniemaesato@gmail.com>
2016-09-29 17:09:50 -0600

    tiny little changes in footer, maybe

--- webpack/components/Footer.js
+++ webpack/components/Footer.js
@@ -18,39 +18,42 @@ class Footer extends Component {
 
   render() {
     return(
-      <div className='container'>
-        <section className='row'>
-          <div className='col-sm-6 col-md-3'>
-            <div className='inner rotate'>
-                <h4 onMouseEnter={() => showMenu()}> Menu </h4>
+      <div>
+        <div className='container'>
+          <section className='row'>
+            <div className='col-sm-6 col-md-3'>
+              <div className='inner rotate'>
+                <h4 onMouseOver={() => showMenu()}> Menu </h4>
+              </div>
             </div>
-          </div>
 
-          <div className='col-sm-6 col-md-3'>
-            <h5> Who We Are </h5>
+            <div className='col-sm-6 col-md-3'>
+              <h5> Who We Are </h5>
 
-            <Link to='/about'>
-            <p> Connecting artist together for creative collaborations.</p>
-            </Link>
+              <Link to='/about'>
+                <p> Connecting artist together for creative collaborations.</p>
+              </Link>
 
-            <p> Thank You For Visiting Our Site!</p>
-            <Link to='/support'>Support</Link>
-          </div>
+              <p> Thank You For Visiting Our Site!</p>
+              <Link to='/support'>Support</Link>
+            </div>
 
-          <div className='col-sm-12 col-md-6'>
-            <h5> Contact Us </h5>
-            <p>
-              Via 370 300 E, Salt Lake City, UT 84111
-              <br />
-              P  801.000.0000
-              <br />
+            <div className='col-sm-12 col-md-6'>
+              <h5> Contact Us </h5>
+              <p>
+                Via 370 300 E, Salt Lake City, UT 84111
+                <br />
+                P  801.000.0000
+                <br />
 
-            </p>
-          </div>
-        </section>
+              </p>
+            </div>
+          </section>
+        </div>
         <div className='nav navbar-bottom'>
-
-          open to collaborate?
+          <div className='container'>
+            open to collaborate?
+          </div>
         </div>
       </div>
     )
