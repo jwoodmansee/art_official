671ac1db54bc2c57ca42922d8fd1211fe315eb03
Alex Quan <alxquan@gmail.com>
2016-09-30 09:45:04 -0600

    put paperclip in the controller instead of the model. whoops

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,7 +1,5 @@
 class Api::ProfilesController < ApiController
   # before_action require: :user
-  has_attached_file :avatar, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   before_action :set_profile, except: [:index, :create]
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,8 @@ class Profile < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_one :category, as: :cat
   after_create :create_profile_category
+  has_attached_file :avatar, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   private
 
