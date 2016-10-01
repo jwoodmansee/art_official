e2ca9eb20faca273b0936a7a470e2ca21e3e2710
Ann Hall <anniemaesato@gmail.com>
2016-10-01 15:12:55 -0600

    working now

--- webpack/components/ProfileInfo.js
+++ webpack/components/ProfileInfo.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react';
 
 class ProfileInfo extends Component {
   render() {
-    let { profile } = this.props.profile;
+    let { bio, inspirations } = this.props.profile;
     return(
       <div className='col-xs-12 col-sm-6 pull-right'>
         <dl className='dl-horizontal'>
           <dd onClick={this.props.toggleEdit} >EDIT PROFILE</dd>
           <dt> bio </dt>
-          <dd> { profile.bio ? profile.bio : 'help collaborators know more about you, add your bio' } </dd>
+          <dd> { bio ? bio : 'help collaborators know more about you, add your bio' } </dd>
           <dt> inspirations </dt>
-          <dd> { profile.inspirations ? profile.inspirations : "let others know what you're about" } </dd>
+          <dd> { inspirations ? inspirations : "let others know what you're about" } </dd>
           <dt> categories </dt>
           {this.props.categories}
         </dl>
