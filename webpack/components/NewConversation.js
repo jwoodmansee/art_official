9802ea63727654789fb008820ac52f885a554b79
Ann Hall <anniemaesato@gmail.com>
2016-10-04 16:40:02 -0600

    add conversation file

---
+++ webpack/components/NewConversation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewConversation extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default NewConversation;
